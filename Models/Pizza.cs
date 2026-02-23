0a96d4ede7c361d7376e53b2cfe37bb2905bf9b7
niraj-sachania <>
2026-02-23 09:42:29 +0000

    fix: correct syntax error in Pizza model

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -4,6 +4,6 @@ public class Pizza
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public bool IsGlutenFree { get; set}
+    public bool IsGlutenFree { get; set; }
 
 }
