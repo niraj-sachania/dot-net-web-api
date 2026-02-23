64c65cfed49f0018331f6ad5e10f420ff23d2a1d
niraj-sachania <>
2026-02-23 09:39:52 +0000

    feat: add a data service

---
+++ Services/PizzaServices.cs
@@ -0,0 +1,43 @@
+using ContossoPizza.Models;
+
+namespace ContossoPizza.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza {Id = 1, Name= "Classic Italian", IsGlutenFree = false},
+            new Pizza {Id = 2, Name="Veggie", IsGlutenFree = true}
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if (pizza is null)
+            return;
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var Index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (Index == -1)
+            return;
+        Pizzas[Index] = pizza;
+    }
+}
